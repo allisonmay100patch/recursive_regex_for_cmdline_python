d2cc543768e6d1a6ec7cfa87522fb9c9fa5a67e6
Allison <105015422+allisonmay100patch@users.noreply.github.com>
2023-12-09 18:48:50 -0500

    Add files via upload

---
+++ regex_cmd_line.py
@@ -0,0 +1,27 @@
+import re
+
+def build_lst(fout,exp):
+    i = []
+
+    for text in fout.readlines():
+        i += re.findall(exp,text)
+        return i
+
+
+def ret_gex():
+    fin = input('input file name')
+    answer = input('final?')
+    exp = input('input exp')
+    return fin,chra,exp
+def do_until(a=[]):
+    fin,answer,exp=ret_gex()
+    
+    with open(fin,'r') as fout:
+        if answer == 'y' or answer == 'yes':
+            fout.close()
+        else:
+            a.append(build_lst(fout,exp))
+            do_until(a=a)
+    return a
+ 
+print(do_until())
