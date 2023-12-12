9e313df70fce9e3ba72d725ba18c5598bd288e5a
Allison <105015422+allisonmay100patch@users.noreply.github.com>
2023-12-11 20:04:06 -0500

    Add files via upload

--- regex_cmd_line.py
+++ regex_cmd_line.py
@@ -1,6 +1,10 @@
 import re
 
 def build_lst(fout,exp):
+    '''
+    returns a match for regexp.
+    '''
+    
     i = []
 
     for text in fout.readlines():
@@ -8,12 +12,22 @@ def build_lst(fout,exp):
         return i
 
 def ret_gex():
+    '''
+    sends input's for do_until to finish, or proceed to the next process (call to other function: dispatch)
+    '''
     fin = input('input file name')
     answer = input('final?')
     exp = input('input exp')
     return fin,answer,exp
     
 def do_until(a=[]):
+    '''
+////->calls ret_gex to ask user for inputs to use regexp functions on files;
+//////////////////////->     opens files one at a time;
+\\\\\\\\\\\\\\\\\\\\\\->     quits on entry of 'ret_gex' returns to 'answer' as 'y' and 'yes' from input;
+    returns list.
+    '''
+    
     fin,answer,exp=ret_gex()
     
     with open(fin,'r') as fout:
@@ -25,4 +39,6 @@ def do_until(a=[]):
             do_until(a=a)
     return a
  
-print(do_until())
+
+if __name__ == "__main__":
+    print(do_until())
